94acf3f74af38578a06f5ee66c7161255b89c217
Ahraz Arfi <ahrazarfi@gmail.com>
2021-03-21 15:09:23 +0530

    update GitTopics.js

--- 4_WebScraping_20_03_21/activity/gitTopics.js
+++ 4_WebScraping_20_03_21/activity/gitTopics.js
@@ -48,13 +48,52 @@ function repo(topicUrl) {
 function extractRepo(html) {
     let gitBaseUrl = "https://github.com";
     let sel = cheerio.load(html);
-    let repoName = sel(".h1-mktg").text();
-    console.log(repoName);
+    let topicName = sel(".h1-mktg").text();
+    console.log(topicName);
     let repoLinkArr = sel(".f3.color-text-secondary.text-normal.lh-condensed .text-bold");
 
     for(let i = 0;i<8;i++){
          let repoLink = gitBaseUrl + sel(repoLinkArr[i]).attr("href");
          let issuesLink = repoLink + "/issues"
-         console.log(repoLink);
+        //  console.log(repoLink);
+        issues(issuesLink, );
     }
+}
+
+
+// issues function, it receives link of each repos issues page.
+function issues(issuesLink) {
+    request(issuesLink, issuesCb);
+
+    // callback function
+    function issuesCb(error, response, html) {
+        if(error){
+            console.log(error);
+        } else{
+            extractIssues(html);
+        }
+    }
+
+    // this function extracts open issues from the github issues page for the specific repo
+    function extractIssues(html) {
+        let sel = cheerio.load(html)
+        let issuesArr = sel("a.Link--primary.v-align-middle.no-underline.h4.js-navigation-open.markdown-title");
+
+        let gitBaseUrl = "https://github.com";
+        let issue = [];
+        // array for open issues
+        for(let i=0;i<issuesArr.length;i++){
+            let issueName = sel(issuesArr[i]).text();
+            let issueLink = gitBaseUrl + sel(issuesArr[i]).attr("href");
+            console.table()
+            let issueObj = {
+                Name : issueName,
+                Link : issueLink
+            }
+            issue.push(issueObj);
+            
+        }
+        console.table(issue);
+    }
+
 }
